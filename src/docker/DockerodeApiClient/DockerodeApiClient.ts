922d9d31fdebff45d16c8d9d447b52eeac71b0d6
Daniel Gospodinow <danielgospodinow@gmail.com>
2020-12-03 19:11:05 +0200

    Revert "Include Docker environment settings (#2490)"
    This reverts commit 37749dd01cd2202961b7a67279c41ac58dfbc801.

--- src/docker/DockerodeApiClient/DockerodeApiClient.ts
+++ src/docker/DockerodeApiClient/DockerodeApiClient.ts
@@ -9,8 +9,6 @@ import * as tar from 'tar';
 import { IActionContext, parseError } from 'vscode-azureextensionui';
 import { CancellationToken } from 'vscode-languageclient';
 import { localize } from '../../localize';
-import { addDockerSettingsToEnv } from '../../utils/addDockerSettingsToEnv';
-import { cloneObject } from '../../utils/cloneObject';
 import { isWindows } from '../../utils/osUtils';
 import { bufferToString, execStreamAsync } from '../../utils/spawnAsync';
 import { DockerInfo, PruneResult } from '../Common';
@@ -82,11 +80,7 @@ export class DockerodeApiClient extends ContextChangeCancelClient implements Doc
 
             dockerCommand += `"${ref}" ${commandProvider('windows').join(' ')}`;
 
-            // Copy the Docker environment settings in
-            let newEnv: NodeJS.ProcessEnv = cloneObject(process.env);
-            addDockerSettingsToEnv(newEnv, process.env);
-
-            const { stdout, stderr } = await execStreamAsync(dockerCommand, { env: newEnv }, token);
+            const { stdout, stderr } = await execStreamAsync(dockerCommand, {}, token);
 
             return { stdout, stderr };
         } else {
