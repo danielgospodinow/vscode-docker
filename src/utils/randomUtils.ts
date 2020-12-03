e1b5c2634ae3ecacdc149cd542f1af802aec19b2
Daniel Gospodinow <danielgospodinow@gmail.com>
2020-12-03 19:11:05 +0200

    Revert "Change to hashed Docker Desktop ID (#2152)"
    This reverts commit 1f85042d085680abb966f30e85ed8625f7684a68.

--- src/utils/randomUtils.ts
+++ src/utils/randomUtils.ts
@@ -3,15 +3,11 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as crypto from 'crypto';
+import * as crypto from "crypto";
 
-export namespace cryptoUtils {
+export namespace randomUtils {
     export function getRandomHexString(length: number = 10): string {
         const buffer: Buffer = crypto.randomBytes(Math.ceil(length / 2));
         return buffer.toString('hex').slice(0, length);
-    }
-
-    export function hashString(input: string): string {
-        return crypto.createHash('sha256').update(input, 'utf8').digest('hex');
     }
 }
